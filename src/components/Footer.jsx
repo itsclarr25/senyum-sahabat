c457fa23600446d98923c7d20479bfd4121f726e
itsclarr25 <clarintapradyan@gmail.com>
2026-05-29 13:22:49 +0700

    update website

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
     <footer className="bg-black text-white py-8 text-center">
-      <p>© 2026 CV. Senyum Sahabat — AC & Refrigeration Specialist</p>
-      <p className="text-gray-400 mt-2">www.senyumsahabat.com</p>
+      <p>© 2026 CV. Senyum Sahabat — Mechanical & Electrical</p>
+      <p className="text-gray-400 mt-2">https://senyum-sahabat.netlify.app/</p>
     </footer>
   );
 }
